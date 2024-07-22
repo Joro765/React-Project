22eeaa38ed54613a7df682510abe07fde80fb534
Georgi <jorogeorgiev765@gmail.com>
2024-07-22 16:58:38 +0300

    Added basic layout css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
         {/* Views */}
       </main>
 
-      {/* Footer */}
+      <Footer />
     </div>
   )
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer className="site-footer">
+            <p>Created by Joro Georgiev</p>
+            <p>All Rights Reserved 2024</p>
+        </footer>
+    )
+}
