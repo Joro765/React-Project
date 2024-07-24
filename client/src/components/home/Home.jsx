9370d30881b42c68bf4fdcf568413cb140e37f6b
Georgi <jorogeorgiev765@gmail.com>
2024-07-24 11:57:47 +0300

    Added Home component
    Fetch and render newest recipes in home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
                 <h2>Newest Recipes</h2>
                 <p>Unleash your inner chef with our latest recipes! Each one is designed to spark creativity and bring fresh flavors to your meals.</p>
             </div>
-            <div className="recipes-list">
+            <div className={styles.recipesList}>
 
                 {recipes.map((recipe) => <RecipeCard key={recipe._id} {...recipe} />)}
             </div>
--- client/src/components/recipe/RecipeCard.jsx
+++ client/src/components/recipe/RecipeCard.jsx
@@ -1,14 +1,17 @@
+import styles from "../recipe/Recipe.module.css";
+
+
 export default function RecipeCard({
     img,
     name,
     description
 }) {
     return (
-        <div className="recipeCard">
-            <div className="imageWrapper">
+        <div className={styles.recipeCard}>
+            <div className={styles.imageWrapper}>
                 <img src={img} alt="" />
             </div>
-            <div className="recipeInfo">
+            <div className={styles.recipeInfo}>
                 <h3>{name}</h3>
                 <p>{description}</p>
             </div>
