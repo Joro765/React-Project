24d7fce30114aa82e462e8bc473772bcdba67ea1
Georgi <jorogeorgiev765@gmail.com>
2024-07-29 19:18:17 +0300

    Added spinner to home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,16 +3,19 @@ import styles from "../home/Home.module.css";
 import { useState, useEffect } from "react";
 import * as recipeApi from "../../api/recipes-api";
 import RecipeCard from "../recipe/recipeCard/RecipeCard";
+import Spinner from "../common/spinner/Spinner";
 
 
 export default function Home() {
     const [recipes, setRecipes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
 
     useEffect(() => {
         async function loadRecipes() {
             const result = await recipeApi.getLatest();
             setRecipes(result);
+            setIsLoading(false);
         }
 
         loadRecipes();
@@ -25,10 +28,14 @@ export default function Home() {
                 <h2>Newest Recipes</h2>
                 <p>Unleash your inner chef with our latest recipes! Each one is designed to spark creativity and bring fresh flavors to your meals.</p>
             </div>
-            <div className={styles.recipesList}>
 
-                {recipes.map((recipe) => <RecipeCard key={recipe._id} {...recipe} />)}
-            </div>
+            {isLoading
+                ? <Spinner />
+                : (
+                    <div className={styles.recipesList}>
+                        {recipes.map((recipe) => <RecipeCard key={recipe._id} {...recipe} />)}
+                    </div>)
+            }
         </div>
     )
 }
