c7c8dbe0217d399addbf0c9234e62f495962763a
Georgi <jorogeorgiev765@gmail.com>
2024-07-31 13:35:17 +0300

    Created useForm custom hook for forms

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,5 +1,27 @@
-export function useForm() {
-    return "";
+import { useState } from "react";
+
+
+export function useForm(intialValues, submitCallback) {
+    const [values, setValues] = useState(intialValues);
+
+
+    function changeHandler(e) {
+        setValues(oldValues => ({ ...oldValues, [e.target.name]: e.target.value }));
+    }
+
+
+    function submitHandler(e) {
+        e.preventDefaul();
+
+        submitCallback(values);
+    }
+
+    return {
+        values,
+        changeHandler,
+        submitHandler,
+
+    }
 }
 
 
